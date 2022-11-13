9ce2a0ceddc4c1ceb76c15488644b96629e88bc4
Paul Boehnke <paulboehnke95@gmail.com>
2022-11-13 12:15:41 +0100

    Automate initial setup mac first steps

---
+++ initial_setup_mac.sh
@@ -0,0 +1,18 @@
+!#/bin/zsh
+
+# Steps to run before running this script:
+# 1. Install Oh-My-ZSH 
+# 2. Clone this dotfiles repository
+
+# Setup links
+rm ~/.zshrc
+rm ~/.vimrc
+ln -s .dotfiles/.zshrc
+ln -s .dotfiles/.vimrc
+ln -s .dotfiles/.p10k.zsh
+
+# install pyenv
+
+# install pyenv-virtualenv
+
+
