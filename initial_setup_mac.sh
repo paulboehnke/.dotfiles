7c22cbc6b601c776efc8a18e1f7fc60f7088b286
Paul Boehnke <paulboehnke95@gmail.com>
2022-11-13 12:53:49 +0100

    Automate more setup steps

--- initial_setup_mac.sh
+++ initial_setup_mac.sh
@@ -2,7 +2,15 @@
 
 # Steps to run before running this script:
 # 1. Install Oh-My-ZSH 
-# 2. Clone this dotfiles repository
+# $ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# 2. Clone this dotfiles repository recursivly
+# git clone --recurse-submodules -j8 git@github.com:paulboehnke/.dotfiles.git
+# 3.Install brew if not already install by other user
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add brew to path
+Set PATH, MANPATH, etc., for Homebrew. >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)" >> ~/.zprofile
 
 # Setup links
 rm ~/.zshrc
@@ -11,8 +19,8 @@ ln -s .dotfiles/.zshrc
 ln -s .dotfiles/.vimrc
 ln -s .dotfiles/.p10k.zsh
 
-# install pyenv
+# install pyenv if not already installed by another user on this machine
 
-# install pyenv-virtualenv
+# install pyenv-virtualenv if not already installed by another user on this machine
 
 
