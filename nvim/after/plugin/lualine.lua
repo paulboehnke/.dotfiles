8d3338d7d3575e1595131fe4a3f0fd8e1e641d91
Paul Boehnke <paulboehnke95@gmail.com>
2023-10-13 09:56:46 +0200

    show full path in lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,3 +1,12 @@
 require('lualine').setup {
   options = { theme  = "16color" },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1
+      }
+    }
+  }
 }
